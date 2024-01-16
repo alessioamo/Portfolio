6633520dae47e4e85b2de9e5bd2416689bbc702e
Alessio <alessio.amorelli@hotmail.com>
2024-01-15 20:09:25 -0500

    Update experiences.params.ts
    - Added relevant experiences information

--- src/lib/experiences.params.ts
+++ src/lib/experiences.params.ts
@@ -6,91 +6,91 @@ const MY_EXPERIENCES: Array<Experience> = [
 	{
 		slug: 'fullstack-web-dev',
 		company: 'Croogloo',
-		description: 'Creating awesome tools for developers.', // TODO
+		description: 'Maintained and updated web application through backend with Google Cloud Console and through frontend with unique features.', // TODO
 		contract: ContractType.Internship,
 		type: 'Web Development',
-		location: 'Home',
-		period: { from: new Date(2023, 1, 0), to: new Date(2023, 5, 0) },
+		location: 'Montréal, Québec',
+		period: { from: new Date(2023, 5, 1), to: new Date(2023, 9, 1) },
 		skills: getSkills('js', 'java', 'html', 'css', 'googlecloud', 'github', 'bootstrap', 'vscode'),
 		name: 'Fullstack Web Developer',
 		color: '#ffffff', // TODO
-		links: [{ to: 'GitHub', label: 'https://github.com', newTab: true },{ to: 'Google', label: 'https://google.com', newTab: true }], // TODO
-		logo: Assets.Croogloo, // TODO
-		shortDescription: 'test' // TODO
+		links: [],
+		logo: Assets.Croogloo,
+		shortDescription: '' // TODO
 	},
 	{
-		slug: 'software-freelance', // TODO
-		company: 'Self-employed', // TODO
-		description: 'Creating awesome applications for customers.', // TODO
-		contract: ContractType.Freelance, // TODO
-		type: 'Software Development', // TODO
-		location: 'Home', // TODO
-		period: { from: new Date() }, // TODO
-		skills: getSkills('python', 'svelte', 'ts', 'sass', 'css', 'html', 'js'), // TODO
-		name: 'Freelancer', // TODO
+		slug: 'technical-publications-developer',
+		company: 'Airbus Canada',
+		description: 'Maintained, updated, and added new functionalities to frontend and backend of technical publications webpages.',
+		contract: ContractType.Internship,
+		type: 'Web Development',
+		location: 'Mirabel, Québec',
+		period: { from: new Date(2022, 5, 1), to: new Date(2022, 9, 1) },
+		skills: getSkills('js', 'java', 'html', 'css', 'bootstrap', 'github', 'eclipse'),
+		name: 'Developer in Technical Publications',
 		color: '#ffffff', // TODO
-		links: [], // TODO
+		links: [],
+		logo: Assets.Airbus,
+		shortDescription: '' // TODO
+	},
+	{
+		slug: 'multimedia-produciton-assistant',
+		company: 'Genetec',
+		description: 'Planned, set up, recorded, and edited videos that demonstrated features and functionalities of software products.',
+		contract: ContractType.Internship,
+		type: 'Software',
+		location: 'Montréal, Québec',
+		period: { from: new Date(2022, 1, 1), to: new Date(2022, 5, 1) },
+		skills: getSkills('premiere', 'photoshop'),
+		name: 'Multimedia Production Assistant',
+		color: '#ffffff', // TODO
+		links: [],
+		logo: Assets.Genetec,
+		shortDescription: '' // TODO
+	},
+	{
+		slug: 'tutor',
+		company: 'Self-employed',
+		description: 'Tutored and aided multiple secondary 3 to 5 students in various subjects such as programming, sciences, and maths. Reached objectives in teaching others using different techniques',
+		contract: ContractType.Freelance,
+		type: 'Tutoring',
+		location: 'Montréal, Québec',
+		period: { from: new Date(2017, 9, 1), to: new Date(2022, 9, 1) },
+		skills: getSkills(),
+		name: 'Tutor',
+		color: '#ffffff', // TODO
+		links: [],
 		logo: Assets.Unknown, // TODO
 		shortDescription: '' // TODO
 	},
 	{
-		slug: 'software-freelance', // TODO
-		company: 'Self-employed', // TODO
-		description: 'Creating awesome applications for customers.', // TODO
-		contract: ContractType.Freelance, // TODO
-		type: 'Software Development', // TODO
-		location: 'Home', // TODO
-		period: { from: new Date() }, // TODO
-		skills: getSkills('python', 'svelte', 'ts', 'sass', 'css', 'html', 'js'), // TODO
-		name: 'Freelancer', // TODO
+		slug: 'camp-counsellor',
+		company: 'Laurus Lifestyles',
+		description: 'Cared for young children with different needs such as autism and ADHD. Encouraged team building through team activities and communication',
+		contract: ContractType.FullTime,
+		type: 'Camp Counsellor',
+		location: 'Montréal, Québec',
+		period: { from: new Date(2019, 5, 1), to: new Date(2019, 9, 1) },
+		skills: getSkills(),
+		name: 'Camp Counsellor',
 		color: '#ffffff', // TODO
-		links: [], // TODO
-		logo: Assets.Unknown, // TODO
+		links: [],
+		logo: Assets.Laurus, // TODO
 		shortDescription: '' // TODO
 	},
 	{
-		slug: 'software-freelance', // TODO
-		company: 'Self-employed', // TODO
-		description: 'Creating awesome applications for customers.', // TODO
-		contract: ContractType.Freelance, // TODO
-		type: 'Software Development', // TODO
-		location: 'Home', // TODO
-		period: { from: new Date() }, // TODO
-		skills: getSkills('python', 'svelte', 'ts', 'sass', 'css', 'html', 'js'), // TODO
-		name: 'Freelancer', // TODO
+		slug: 'student-works',
+		company: 'Student Works Painting',
+		description: 'Marketed and provided painting services with a dedicated team.',
+		contract: ContractType.FullTime,
+		type: 'Painter and Customer Service',
+		location: 'Montréal, Québec',
+		period: { from: new Date(2018, 1, 1), to: new Date(2018, 9, 1) },
+		skills: getSkills(),
+		name: 'Painter and Customer Service',
 		color: '#ffffff', // TODO
-		links: [], // TODO
-		logo: Assets.Unknown, // TODO
-		shortDescription: '' // TODO
-	},
-	{
-		slug: 'software-freelance', // TODO
-		company: 'Self-employed', // TODO
-		description: 'Creating awesome applications for customers.', // TODO
-		contract: ContractType.Freelance, // TODO
-		type: 'Software Development', // TODO
-		location: 'Home', // TODO
-		period: { from: new Date() }, // TODO
-		skills: getSkills('python', 'svelte', 'ts', 'sass', 'css', 'html', 'js'), // TODO
-		name: 'Freelancer', // TODO
-		color: '#ffffff', // TODO
-		links: [], // TODO
-		logo: Assets.Unknown, // TODO
-		shortDescription: '' // TODO
-	},
-	{
-		slug: 'software-freelance', // TODO
-		company: 'Self-employed', // TODO
-		description: 'Creating awesome applications for customers.', // TODO
-		contract: ContractType.Freelance, // TODO
-		type: 'Software Development', // TODO
-		location: 'Home', // TODO
-		period: { from: new Date() }, // TODO
-		skills: getSkills('python', 'svelte', 'ts', 'sass', 'css', 'html', 'js'), // TODO
-		name: 'Freelancer', // TODO
-		color: '#ffffff', // TODO
-		links: [], // TODO
-		logo: Assets.Unknown, // TODO
+		links: [],
+		logo: Assets.StudentWorks,
 		shortDescription: '' // TODO
 	}
 ];
