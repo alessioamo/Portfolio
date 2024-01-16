f9ef537d966253639153df5de7d8cae2e46eb978
Alessio <alessio.amorelli@hotmail.com>
2024-01-15 22:58:33 -0500

    Project Date Test
    - Re-added dates to projects to test

--- src/lib/projects.params.ts
+++ src/lib/projects.params.ts
@@ -13,207 +13,9 @@ const MY_PROJECTS: Array<Project> = [
 		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
 		logo: Assets.Unknown, //TODO
 		name: 'Slick Portfolio', //TODO
-		// period: {
-		// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
-		// },
-		skills: getSkills('angular', 'ts', 'tailwind'), //TODO
-		type: 'Website Template', //TODO
-		screenshots: [ //TODO
-			{
-				label: 'screen 1',
-				src: ''
-			}
-		]
-	},
-	{
-		slug: 'slick-portfolio-angular', //TODO
-		color: '#a300de', //TODO
-		description:
-			'Lorem ipsum', //TODO
-		shortDescription:
-			'Lorem', //TODO
-		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
-		logo: Assets.Unknown, //TODO
-		name: 'Slick Portfolio', //TODO
-		// period: {
-		// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
-		// },
-		skills: getSkills('angular', 'ts', 'tailwind'), //TODO
-		type: 'Website Template', //TODO
-		screenshots: [ //TODO
-			{
-				label: 'screen 1',
-				src: ''
-			}
-		]
-	},
-	{
-		slug: 'slick-portfolio-angular', //TODO
-		color: '#a300de', //TODO
-		description:
-			'Lorem ipsum', //TODO
-		shortDescription:
-			'Lorem', //TODO
-		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
-		logo: Assets.Unknown, //TODO
-		name: 'Slick Portfolio', //TODO
-		// period: {
-		// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
-		// },
-		skills: getSkills('angular', 'ts', 'tailwind'), //TODO
-		type: 'Website Template', //TODO
-		screenshots: [ //TODO
-			{
-				label: 'screen 1',
-				src: ''
-			}
-		]
-	},
-	{
-		slug: 'slick-portfolio-angular', //TODO
-		color: '#a300de', //TODO
-		description:
-			'Lorem ipsum', //TODO
-		shortDescription:
-			'Lorem', //TODO
-		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
-		logo: Assets.Unknown, //TODO
-		name: 'Slick Portfolio', //TODO
-		// period: {
-		// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
-		// },
-		skills: getSkills('angular', 'ts', 'tailwind'), //TODO
-		type: 'Website Template', //TODO
-		screenshots: [ //TODO
-			{
-				label: 'screen 1',
-				src: ''
-			}
-		]
-	},
-	{
-		slug: 'slick-portfolio-angular', //TODO
-		color: '#a300de', //TODO
-		description:
-			'Lorem ipsum', //TODO
-		shortDescription:
-			'Lorem', //TODO
-		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
-		logo: Assets.Unknown, //TODO
-		name: 'Slick Portfolio', //TODO
-		// period: {
-		// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
-		// },
-		skills: getSkills('angular', 'ts', 'tailwind'), //TODO
-		type: 'Website Template', //TODO
-		screenshots: [ //TODO
-			{
-				label: 'screen 1',
-				src: ''
-			}
-		]
-	},
-	{
-		slug: 'slick-portfolio-angular', //TODO
-		color: '#a300de', //TODO
-		description:
-			'Lorem ipsum', //TODO
-		shortDescription:
-			'Lorem', //TODO
-		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
-		logo: Assets.Unknown, //TODO
-		name: 'Slick Portfolio', //TODO
-		// period: {
-		// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
-		// },
-		skills: getSkills('angular', 'ts', 'tailwind'), //TODO
-		type: 'Website Template', //TODO
-		screenshots: [ //TODO
-			{
-				label: 'screen 1',
-				src: ''
-			}
-		]
-	},
-	{
-		slug: 'slick-portfolio-angular', //TODO
-		color: '#a300de', //TODO
-		description:
-			'Lorem ipsum', //TODO
-		shortDescription:
-			'Lorem', //TODO
-		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
-		logo: Assets.Unknown, //TODO
-		name: 'Slick Portfolio', //TODO
-		// period: {
-		// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
-		// },
-		skills: getSkills('angular', 'ts', 'tailwind'), //TODO
-		type: 'Website Template', //TODO
-		screenshots: [ //TODO
-			{
-				label: 'screen 1',
-				src: ''
-			}
-		]
-	},
-	{
-		slug: 'slick-portfolio-angular', //TODO
-		color: '#a300de', //TODO
-		description:
-			'Lorem ipsum', //TODO
-		shortDescription:
-			'Lorem', //TODO
-		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
-		logo: Assets.Unknown, //TODO
-		name: 'Slick Portfolio', //TODO
-		// period: {
-		// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
-		// },
-		skills: getSkills('angular', 'ts', 'tailwind'), //TODO
-		type: 'Website Template', //TODO
-		screenshots: [ //TODO
-			{
-				label: 'screen 1',
-				src: ''
-			}
-		]
-	},
-	{
-		slug: 'slick-portfolio-angular', //TODO
-		color: '#a300de', //TODO
-		description:
-			'Lorem ipsum', //TODO
-		shortDescription:
-			'Lorem', //TODO
-		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
-		logo: Assets.Unknown, //TODO
-		name: 'Slick Portfolio', //TODO
-		// period: {
-		// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
-		// },
-		skills: getSkills('angular', 'ts', 'tailwind'), //TODO
-		type: 'Website Template', //TODO
-		screenshots: [ //TODO
-			{
-				label: 'screen 1',
-				src: ''
-			}
-		]
-	},
-	{
-		slug: 'slick-portfolio-angular', //TODO
-		color: '#a300de', //TODO
-		description:
-			'Lorem ipsum', //TODO
-		shortDescription:
-			'Lorem', //TODO
-		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
-		logo: Assets.Unknown, //TODO
-		name: 'Slick Portfolio', //TODO
-		// period: {
-		// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
-		// },
+		period: {
+			from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
+		},
 		skills: getSkills('angular', 'ts', 'tailwind'), //TODO
 		type: 'Website Template', //TODO
 		screenshots: [ //TODO
@@ -223,6 +25,204 @@ const MY_PROJECTS: Array<Project> = [
 			}
 		]
 	}
+	// {
+	// 	slug: 'slick-portfolio-angular', //TODO
+	// 	color: '#a300de', //TODO
+	// 	description:
+	// 		'Lorem ipsum', //TODO
+	// 	shortDescription:
+	// 		'Lorem', //TODO
+	// 	links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
+	// 	logo: Assets.Unknown, //TODO
+	// 	name: 'Slick Portfolio', //TODO
+	// 	// period: {
+	// 	// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
+	// 	// },
+	// 	skills: getSkills('angular', 'ts', 'tailwind'), //TODO
+	// 	type: 'Website Template', //TODO
+	// 	screenshots: [ //TODO
+	// 		{
+	// 			label: 'screen 1',
+	// 			src: ''
+	// 		}
+	// 	]
+	// },
+	// {
+	// 	slug: 'slick-portfolio-angular', //TODO
+	// 	color: '#a300de', //TODO
+	// 	description:
+	// 		'Lorem ipsum', //TODO
+	// 	shortDescription:
+	// 		'Lorem', //TODO
+	// 	links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
+	// 	logo: Assets.Unknown, //TODO
+	// 	name: 'Slick Portfolio', //TODO
+	// 	// period: {
+	// 	// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
+	// 	// },
+	// 	skills: getSkills('angular', 'ts', 'tailwind'), //TODO
+	// 	type: 'Website Template', //TODO
+	// 	screenshots: [ //TODO
+	// 		{
+	// 			label: 'screen 1',
+	// 			src: ''
+	// 		}
+	// 	]
+	// },
+	// {
+	// 	slug: 'slick-portfolio-angular', //TODO
+	// 	color: '#a300de', //TODO
+	// 	description:
+	// 		'Lorem ipsum', //TODO
+	// 	shortDescription:
+	// 		'Lorem', //TODO
+	// 	links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
+	// 	logo: Assets.Unknown, //TODO
+	// 	name: 'Slick Portfolio', //TODO
+	// 	// period: {
+	// 	// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
+	// 	// },
+	// 	skills: getSkills('angular', 'ts', 'tailwind'), //TODO
+	// 	type: 'Website Template', //TODO
+	// 	screenshots: [ //TODO
+	// 		{
+	// 			label: 'screen 1',
+	// 			src: ''
+	// 		}
+	// 	]
+	// },
+	// {
+	// 	slug: 'slick-portfolio-angular', //TODO
+	// 	color: '#a300de', //TODO
+	// 	description:
+	// 		'Lorem ipsum', //TODO
+	// 	shortDescription:
+	// 		'Lorem', //TODO
+	// 	links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
+	// 	logo: Assets.Unknown, //TODO
+	// 	name: 'Slick Portfolio', //TODO
+	// 	// period: {
+	// 	// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
+	// 	// },
+	// 	skills: getSkills('angular', 'ts', 'tailwind'), //TODO
+	// 	type: 'Website Template', //TODO
+	// 	screenshots: [ //TODO
+	// 		{
+	// 			label: 'screen 1',
+	// 			src: ''
+	// 		}
+	// 	]
+	// },
+	// {
+	// 	slug: 'slick-portfolio-angular', //TODO
+	// 	color: '#a300de', //TODO
+	// 	description:
+	// 		'Lorem ipsum', //TODO
+	// 	shortDescription:
+	// 		'Lorem', //TODO
+	// 	links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
+	// 	logo: Assets.Unknown, //TODO
+	// 	name: 'Slick Portfolio', //TODO
+	// 	// period: {
+	// 	// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
+	// 	// },
+	// 	skills: getSkills('angular', 'ts', 'tailwind'), //TODO
+	// 	type: 'Website Template', //TODO
+	// 	screenshots: [ //TODO
+	// 		{
+	// 			label: 'screen 1',
+	// 			src: ''
+	// 		}
+	// 	]
+	// },
+	// {
+	// 	slug: 'slick-portfolio-angular', //TODO
+	// 	color: '#a300de', //TODO
+	// 	description:
+	// 		'Lorem ipsum', //TODO
+	// 	shortDescription:
+	// 		'Lorem', //TODO
+	// 	links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
+	// 	logo: Assets.Unknown, //TODO
+	// 	name: 'Slick Portfolio', //TODO
+	// 	// period: {
+	// 	// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
+	// 	// },
+	// 	skills: getSkills('angular', 'ts', 'tailwind'), //TODO
+	// 	type: 'Website Template', //TODO
+	// 	screenshots: [ //TODO
+	// 		{
+	// 			label: 'screen 1',
+	// 			src: ''
+	// 		}
+	// 	]
+	// },
+	// {
+	// 	slug: 'slick-portfolio-angular', //TODO
+	// 	color: '#a300de', //TODO
+	// 	description:
+	// 		'Lorem ipsum', //TODO
+	// 	shortDescription:
+	// 		'Lorem', //TODO
+	// 	links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
+	// 	logo: Assets.Unknown, //TODO
+	// 	name: 'Slick Portfolio', //TODO
+	// 	// period: {
+	// 	// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
+	// 	// },
+	// 	skills: getSkills('angular', 'ts', 'tailwind'), //TODO
+	// 	type: 'Website Template', //TODO
+	// 	screenshots: [ //TODO
+	// 		{
+	// 			label: 'screen 1',
+	// 			src: ''
+	// 		}
+	// 	]
+	// },
+	// {
+	// 	slug: 'slick-portfolio-angular', //TODO
+	// 	color: '#a300de', //TODO
+	// 	description:
+	// 		'Lorem ipsum', //TODO
+	// 	shortDescription:
+	// 		'Lorem', //TODO
+	// 	links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
+	// 	logo: Assets.Unknown, //TODO
+	// 	name: 'Slick Portfolio', //TODO
+	// 	// period: {
+	// 	// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
+	// 	// },
+	// 	skills: getSkills('angular', 'ts', 'tailwind'), //TODO
+	// 	type: 'Website Template', //TODO
+	// 	screenshots: [ //TODO
+	// 		{
+	// 			label: 'screen 1',
+	// 			src: ''
+	// 		}
+	// 	]
+	// },
+	// {
+	// 	slug: 'slick-portfolio-angular', //TODO
+	// 	color: '#a300de', //TODO
+	// 	description:
+	// 		'Lorem ipsum', //TODO
+	// 	shortDescription:
+	// 		'Lorem', //TODO
+	// 	links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }], //TODO
+	// 	logo: Assets.Unknown, //TODO
+	// 	name: 'Slick Portfolio', //TODO
+	// 	// period: {
+	// 	// 	from: new Date(2018, 8, 1), to: new Date(2020, 8, 1) //TODO
+	// 	// },
+	// 	skills: getSkills('angular', 'ts', 'tailwind'), //TODO
+	// 	type: 'Website Template', //TODO
+	// 	screenshots: [ //TODO
+	// 		{
+	// 			label: 'screen 1',
+	// 			src: ''
+	// 		}
+	// 	]
+	// }
 	
 ];
 
