651659c5f97239ee7891fe341d107b3ff1dc96fb
Alessio <80137378+alessioamo@users.noreply.github.com>
2025-03-20 15:09:22 -0400

    Initial Conversion to Svelte 5

--- src/routes/projects/[slug]/+page.ts
+++ src/routes/projects/[slug]/+page.ts
@@ -1,11 +1,11 @@
-import MY_PROJECTS from '$lib/projects.params';
+import ProjectsData from '$lib/data/projects';
 
 export function load({ params }: { params: Record<string, string> }) {
 	if (params.slug) {
-		const project = MY_PROJECTS.find((item) => {
+		const item = ProjectsData.items.find((item) => {
 			return item.slug === params.slug;
 		});
 
-		return { project };
+		return { item };
 	}
 }
