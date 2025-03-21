26181b71e9a1e249dfe0f846a2fbb558856fecc8
Alessio <80137378+alessioamo@users.noreply.github.com>
2025-03-21 02:02:43 -0400

    Update screenshot-card.svelte

--- src/lib/components/common/screenshot/screenshot-card.svelte
+++ src/lib/components/common/screenshot/screenshot-card.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Button from '$lib/components/ui/button/button.svelte';
 	import { Card, CardFooter } from '$lib/components/ui/card';
+
 	import {
 		Dialog,
 		DialogClose,
@@ -32,12 +33,11 @@
 		<div
 			class="flex-1 bg-cover bg-center bg-no-repeat"
 			style={`background-image: url("${item.src}"); background-size: contain; background-position: center center;`}
-		>
-		</div>
+		></div>
+		<DialogFooter>
+			<DialogClose>
+				<Button>Close</Button>
+			</DialogClose>
+		</DialogFooter>
 	</DialogContent>
-	<DialogFooter>
-		<DialogClose>
-			<Button>Close</Button>
-		</DialogClose>
-	</DialogFooter>
 </Dialog>
