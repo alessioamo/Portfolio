1518ae96eaeb2b09b372b8b95a3ef84c4f133ab0
Alessio <80137378+alessioamo@users.noreply.github.com>
2025-03-21 00:25:20 -0400

    test

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -11,11 +11,12 @@ const hero: {
 } = {
 	title: `${BaseData.fullName},`,
 	description:
-		'Welcome to my Portfolio! It is currently under development and is being updated so come back soon to see the final version! In the meantime, ' + 
-		'feel free to explore the sections that are complete!',
+		'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
 	links: [
-		{ label: 'GitHub', href: 'https://github.com/alessioamo', icon: 'i-carbon-logo-github' },
-		{ label: 'LinkedIn', href: 'https://www.linkedin.com/in/alessio-amorelli-9702211aa/', icon: 'i-carbon-logo-linkedin' }
+		{ label: 'GitHub', href: 'https://github.com', icon: 'i-carbon-logo-github' },
+		{ label: 'LinkedIn', href: 'https://linkedin.com', icon: 'i-carbon-logo-linkedin' },
+		{ label: 'Twitter', href: 'https://twitter.com', icon: 'i-carbon-logo-twitter' },
+		{ label: 'Email', href: 'mailto:riadh.adrani@pm.me', icon: 'i-carbon-at' }
 	]
 };
 
